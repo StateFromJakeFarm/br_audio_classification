9ab4907d256c2660ac1150bf19001bf6ff70db01
Jake Hansen <jhansen44@mail.csuchico.edu>
2017-12-20 22:56:57 -0800

    playing with parse()

--- soundScrape/soundScrape/spiders/sound_spider.py
+++ soundScrape/soundScrape/spiders/sound_spider.py
@@ -1,18 +1,62 @@
 import scrapy
 import re
+import logging
 from bs4 import BeautifulSoup
 
 class SoundSpider(scrapy.Spider):
     name = "sound"
 
+    sound_file_types = [
+        'mp3',
+        'wav'
+    ]
+
+    base_url_types = [
+        '.com',
+        '.org',
+        '.gov',
+        '.co.uk'
+    ]
+
+    def build_regex(self):
+        if len(self.sound_file_types) <= 0:
+            logging.error('Must specify at least one audio file type')
+            exit(1)
+
+        regex = ''
+        for i, extension in enumerate(self.sound_file_types):
+            if i > 0:
+                regex += '|'
+            regex += '(.*\.' + extension + ')'
+
+        return regex
+
     def start_requests(self):
         urls = [
-            'http://soundbible.com/tags-chain.html'
+            'http://soundbible.com/tags-chain.html',
+            'http://www.freesfx.co.uk/sfx/saw'
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback = self.parse)
 
     def parse(self, response):
         soup = BeautifulSoup(response.body)
-        for a in soup.findAll('a', href = re.compile('http.*\.mp3')):
-            self.log('Found sound file: ' + a['href'])
+
+        # Heuristically guess the base URL
+        base_url = ''
+        for base_url_type in self.base_url_types:
+            pos = response.url.find(base_url_type)
+            if pos != -1:
+                base_url = response.url[0:(pos + len(base_url_type))]
+
+        for a in soup.findAll('a', href = re.compile( self.build_regex() )):
+            link = a['href']
+            if base_url not in link:
+                if base_url[-1] == '/' and link[0] == '/':
+                    link = base_url[:-1] + link
+                elif base_url[-1] != '/' and link[0] != '/':
+                    link = base_url + '/' + link
+                else:
+                    link = base_url + link
+
+            logging.info('Found sound file: ' + link)
