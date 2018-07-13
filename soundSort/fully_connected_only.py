c4143fcaedb619a7d75b04f3a58758bde7d00557
Jake Hansen <jhansen44@mail.csuchico.edu>
2018-07-13 01:02:14 -0700

    update fully_connected_only

--- soundSort/fully_connected_only.py
+++ soundSort/fully_connected_only.py
@@ -1,5 +1,6 @@
 import glob
 import os
+import logging
 import librosa
 import librosa.display
 import matplotlib.pyplot as plt
@@ -8,3 +9,15 @@ import tensorflow as tf
 
 from matplotlib.pyplot import specgram
 from GCS_data_manager import GCS_data_manager
+
+logging.basicConfig(level=logging.INFO)
+
+def load_sound_files(file_paths):
+    raw_sounds = []
+    for path in file_paths:
+        x, sr = librosa.load(path)
+        raw_sounds.append(x)
+
+    return raw_sounds
+
+dm = GCS_data_manager('../soundScrape-d78c4b542d68.json', 'soundscrape-bucket')
