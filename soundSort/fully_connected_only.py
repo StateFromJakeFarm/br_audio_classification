bf60c9328e609789947834b6678c03fcaf200327
Jake Hansen <jhansen44@mail.csuchico.edu>
2018-07-13 00:59:36 -0700

    begin fully_connected_only

---
+++ soundSort/fully_connected_only.py
@@ -0,0 +1,10 @@
+import glob
+import os
+import librosa
+import librosa.display
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+
+from matplotlib.pyplot import specgram
+from GCS_data_manager import GCS_data_manager
