c7cf34de279c47320e82aa08cdf5812b86987de4
Jake Hansen <jake.musiclover.hansen@gmail.com>
2019-03-03 19:03:38 -0800

    LSTM autoencoder writes decompressed WAV

--- soundSort/autoencoder_lstm.py
+++ soundSort/autoencoder_lstm.py
@@ -4,21 +4,22 @@ import torch.optim as optim
 import numpy as np
 import logging
 
+from scipy.io.wavfile import write
 from SoundSortDataManager import SoundSortDataManager as ssdm
 
 logging.getLogger().setLevel(logging.INFO)
 
 # High-level params
-sample_rate = 8000
-batch_size = 4000
+sample_rate = 4000
+batch_size = 1000
 num_batches = sample_rate / batch_size
 duration = 2
-num_epochs = 200
+num_epochs = 100
 input_dim = batch_size
 hidden_dim = int(batch_size * 1.25)
 
 class AutoEncoder(nn.Module):
-    def __init__(self, input_dim, hidden_dim):
+    def __init__(self, input_dim, hidden_dim, use_cuda):
         super(AutoEncoder, self).__init__()
         self.lstm1 = nn.LSTMCell(input_dim, hidden_dim)
         self.lstm2 = nn.LSTMCell(hidden_dim, hidden_dim)
@@ -32,6 +33,11 @@ class AutoEncoder(nn.Module):
         self.h2_t = torch.zeros(1, hidden_dim, dtype=torch.float)
         self.c2_t = torch.zeros(1, hidden_dim, dtype=torch.float)
 
+        if use_cuda:
+            self.h_t = self.h_t.cuda()
+            self.c_t = self.c_t.cuda()
+            self.h2_t = self.h2_t.cuda()
+            self.c2_t = self.c2_t.cuda()
 
     def forward(self, input_data):
         self.h_t, self.c_t = self.lstm1(input_data, (self.h_t, self.c_t))
@@ -46,10 +52,18 @@ dm = ssdm('soundScrapeDumps', '../soundScrape-d78c4b542d68.json',
 dm.prep_data(file_name='wind-metal-wind-metal_69699137754365d68f61c826163d29e16ec7c901.wav')
 
 # Create network
-auto_encoder = AutoEncoder(input_dim, hidden_dim)
-auto_encoder.float()
-loss_function = nn.MSELoss()
+use_cuda = (torch.cuda.device_count() > 1)
+auto_encoder = AutoEncoder(input_dim, hidden_dim, use_cuda)
+loss_function = nn.L1Loss()
 optimizer = optim.SGD(auto_encoder.parameters(), lr=0.001)
+
+device = torch.device('cpu')
+if use_cuda:
+    auto_encoder = torch.nn.DataParallel(auto_encoder, device_ids=[0])
+    device = torch.device('cuda:0')
+
+auto_encoder.to(device)
+auto_encoder.float()
 
 # Train network
 for epoch in range(num_epochs):
@@ -57,10 +71,11 @@ for epoch in range(num_epochs):
         chunk = dm.next_chunk()
 
         for batch in np.split(chunk, num_batches):
-            print(chunk)
-            print(batch)
             fft = np.fft.fft(batch).real.astype(np.float)
             fft_tensor = torch.from_numpy(fft).float().unsqueeze(0)
+
+            if use_cuda:
+                fft_tensor = fft_tensor.cuda()
 
             auto_encoder.zero_grad()
 
@@ -76,3 +91,21 @@ for epoch in range(num_epochs):
 
         if sec == duration-1:
             logging.info('{}/{} loss = {}'.format(epoch+1, num_epochs, loss))
+
+# Write decompressed version back to wavfile
+dm.i = 0 # Go back to beginning of file
+wav_data = np.empty(1, dtype=np.float64)
+for sec in range(duration):
+    chunk = dm.next_chunk()
+    for batch in np.split(chunk, num_batches):
+        fft = np.fft.fft(batch).real.astype(np.float)
+        fft_tensor = torch.from_numpy(fft).float().unsqueeze(0)
+
+        output = auto_encoder(fft_tensor)
+        if use_cuda:
+            output = output.cpu()
+        output = output.squeeze(0).detach().numpy()
+
+        wav_data = np.concatenate((wav_data, np.fft.ifft(output).real.astype(np.float)))
+
+write('autoencoder_output.wav', sample_rate, wav_data)
