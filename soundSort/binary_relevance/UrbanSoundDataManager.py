9acf0536933acc709d21d2dadd5d1ce1a9e3ad32
Jake Hansen <jhansen44@mail.csuchico.edu>
2019-04-02 11:04:24 -0700

    It works!

--- soundSort/binary_relevance/UrbanSoundDataManager.py
+++ soundSort/binary_relevance/UrbanSoundDataManager.py
@@ -37,17 +37,21 @@ class UrbanSoundDataManager:
         else:
             raise ValueError('test_fold must be in range [1, 10]')
 
-        # Compile list of all training file paths
+        # Compile lists of all training and testing file paths
         self.train_files = []
+        self.test_files = []
         for fold_num in range(1, 11):
-            if fold_num == self.test_fold:
-                # Don't add testing samples to training set
-                continue
-
             fold = 'fold' + str(fold_num)
             fold_path = os.path.join(audio_dir, fold)
             for f_name in os.listdir(fold_path):
-                self.train_files.append(os.path.join(fold_path, f_name))
+                f_path = os.path.join(fold_path, f_name)
+
+                if fold_num == self.test_fold:
+                    # Belongs in testing set
+                    self.test_files.append(f_path)
+                else:
+                    # Belongs in training set
+                    self.train_files.append(f_path)
 
         # Shuffle training set order
         random.shuffle(self.train_files)
@@ -58,23 +62,42 @@ class UrbanSoundDataManager:
         self.chunk_len = int(chunk_duration*self.sr)
         self.total_samples = self.sr*file_duration
 
-        # Iterator to keep track of where we are in the training set
-        self.i = 0
+        # Iterators to keep track of where we are in the training and testing sets
+        self.i_train = 0
+        self.i_test = 0
 
-    def get_batch(self, size=10):
+    def get_batch(self, type, size=10):
         '''
         Get next batch of shape (batch, seq_len, seq), which is representative
         of (file, chunks, chunk_len)
         '''
+        # Gross hack to actually increment the iterator for the set being used
+        if type == 'train':
+            file_set = self.train_files
+            iterator = self.i_train
+
+            # Increment iterator
+            self.i_train += size
+            if self.i_train + size >= len(self.train_files):
+                self.i_train = 0
+        else:
+            file_set = self.test_files
+            iterator = self.i_test
+
+            # Increment iterator
+            self.i_test += size
+            if self.i_test + size >= len(self.test_files):
+                self.i_test = 0
+
         # Compile file data for this chunk into tensor
         batch = np.zeros((size, self.chunks, self.chunk_len), dtype=float)
         labels = []
-        for i, train_file in enumerate(self.train_files[self.i:self.i+size]):
+        for i, file in enumerate(file_set[iterator:iterator+size]):
             # Extract label
-            labels.append(int(os.path.split(train_file)[-1].split('-')[1]))
+            labels.append(int(os.path.split(file)[-1].split('-')[1]))
 
             # Load data
-            Y, sr = librosa.core.load(train_file, sr=self.sr, duration=self.file_duration)
+            Y, sr = librosa.core.load(file, sr=self.sr, duration=self.file_duration)
 
             if Y.shape[0] < self.total_samples:
                 # Pad this array with zeros on the end
@@ -83,10 +106,5 @@ class UrbanSoundDataManager:
             # Chunk-up data
             for chunk in range(self.chunks):
                 batch[i][chunk] = Y[chunk*self.chunk_len:chunk*self.chunk_len+self.chunk_len]
-
-        # Increment iterator
-        self.i += size
-        if self.i + size >= len(self.train_files):
-            self.i = 0
 
         return torch.from_numpy(batch).float(), labels
--- soundSort/binary_relevance/classifier.py
+++ soundSort/binary_relevance/classifier.py
@@ -6,6 +6,8 @@ import logging
 
 from UrbanSoundDataManager import UrbanSoundDataManager
 
+logging.getLogger().setLevel(logging.INFO)
+
 class Classifier:
     '''
     Train LSTM models to identify each class of the
@@ -48,22 +50,51 @@ class Classifier:
 
             return x
 
-    def __init__(self, audio_dir, hidden_size, batch_size):
+    def __init__(self, audio_dir, hidden_size, batch_size, lr=0.005):
         self.dm = UrbanSoundDataManager(audio_dir)
         self.batch_size = batch_size
 
+        # Loss function used during training
+        self.loss_function = nn.L1Loss()
+
         self.models = []
+        self.optimizers = []
         for label in range(1, 11):
             model = self.Model(self.dm.chunk_len, hidden_size, batch_size, self.dm.chunks, self.dm.chunk_len, label)
             model.float()
             self.models.append(model)
 
+            # Optimizers specific to each model
+            self.optimizers.append(optim.SGD(model.parameters(), lr=lr))
+
     def train(self, epochs):
         for e in range(epochs):
-            batch, labels = self.dm.get_batch()
+            batch, labels = self.dm.get_batch('train')
 
-            for model in self.models:
+            for i in range(len(self.models)):
+                model = self.models[i]
+                optimizer = self.optimizers[i]
+
+                # Run network
                 output = model(batch)
 
-classifier = Classifier('/home/jakeh/Downloads/UrbanSound8K/audio', 800, 10)
+                # Determine correct output for this batch
+                correct_output = torch.Tensor([float(label == model.label) for label in labels])
+
+                # Calculate loss
+                loss = self.loss_function(output, correct_output)
+
+                # Backpropagate
+                loss.backward(retain_graph=True)
+                optimizer.step()
+
+                if (e+1) % (epochs/10) == 0:
+                    # Run against test set
+                    batch, labels = self.dm.get_batch('test')
+                    output = model(batch).detach().numpy()
+                    correct_output = np.array([float(label == model.label) for label in labels])
+
+                    logging.info('({}/{}) model {}: error = {}'.format(e+1, epochs, i+1, np.mean(np.abs(output - correct_output))))
+
+classifier = Classifier('/home/jakeh/Downloads/UrbanSound8K/audio', 800, 100)
 classifier.train(10)
