cd0c9c637d81fda4e758d300cab23751a22538e2
Jake Hansen <jake.musiclover.hansen@gmail.com>
2019-04-07 19:24:13 -0700

    Adds test() method

--- soundSort/binary_relevance/classifier.py
+++ soundSort/binary_relevance/classifier.py
@@ -8,10 +8,10 @@ from UrbanSoundDataManager import UrbanSoundDataManager
 from sys import argv, stderr
 
 # Model params
-hidden_dim = 800
+hidden_dim = 400
 batch_dim = 100
 lr = 0.005
-epochs = 50
+epochs = 10
 
 class Classifier:
     '''
@@ -23,10 +23,10 @@ class Classifier:
         Model used to identify each class
         '''
         def init_state_tensors(self):
-            self.h = torch.randn(1, self.batch_size, self.hidden_size)
-            self.c = torch.randn(1, self.batch_size, self.hidden_size)
+            self.h = torch.randn(1, self.batch_size, self.hidden_size).to(self.device)
+            self.c = torch.randn(1, self.batch_size, self.hidden_size).to(self.device)
 
-        def __init__(self, input_size, hidden_size, batch_size, chunks, chunk_len, label):
+        def __init__(self, input_size, hidden_size, batch_size, chunks, chunk_len, label, device):
             super(Classifier.Model, self).__init__()
 
             self.hidden_size = hidden_size
@@ -34,6 +34,7 @@ class Classifier:
             self.chunks = chunks
             self.chunk_len = chunk_len
             self.label = label
+            self.device = device
 
             # Define model
             self.lstm = nn.LSTM(input_size, hidden_size, batch_first=True)
@@ -82,28 +83,58 @@ class Classifier:
         self.models = []
         self.optimizers = []
         for label in range(1, 11):
-            model = self.Model(self.dm.chunk_len, hidden_size, batch_size, self.dm.chunks, self.dm.chunk_len, label)
+            model = self.Model(self.dm.chunk_len, hidden_size, batch_size, self.dm.chunks, self.dm.chunk_len, label, self.device)
             model.float()
 
             if self.use_cuda:
                 # Prep model to train on GPU
+                label = model.label
                 model = nn.DataParallel(model, device_ids=self.device_ids)
+
+                # Hack to get DataParallel object to maintain Model object's label
+                setattr(model, 'label', label)
 
             self.models.append(model)
 
             # Optimizers specific to each model
             self.optimizers.append(optim.SGD(model.parameters(), lr=lr))
 
+    def test(self, model):
+        '''
+        Determine a model's accuracy against testing set
+        '''
+        total_test_files = len(self.dm.test_files)
+        num_test_files = total_test_files - (total_test_files % self.batch_size)
+
+        c_true, o_true = 0, 0
+        num_batches = num_test_files//self.batch_size
+        for i in range(num_batches):
+            # Get testing batch
+            batch, labels = self.dm.get_batch('test', size=self.batch_size)
+            batch.to(self.device)
+            labels = np.array(labels)
+
+            # Run model
+            rounded_output = np.rint(model(batch).detach().cpu().numpy())
+
+            # Get number of mislabeled files
+            c_true += np.sum(labels)
+            o_true += np.sum(rounded_output)
+
+        return c_true, o_true
+
     def train(self, epochs):
         logging.info('Begin training')
-        for e in range(epochs):
-            batch, labels = self.dm.get_batch('train', size=self.batch_size)
-            batch.to(self.device)
 
-            for i in range(len(self.models)):
-                # Retrieve model
-                model = self.models[i]
-                model.to(self.device)
+        for i in range(len(self.models)):
+            # Retrieve model
+            model = self.models[i]
+            model.to(self.device)
+
+            for e in range(epochs):
+                # Retrieve batch
+                batch, labels = self.dm.get_batch('train', size=self.batch_size)
+                batch.to(self.device)
 
                 # Retrieve optimizer
                 optimizer = self.optimizers[i]
@@ -123,11 +154,8 @@ class Classifier:
 
                 if (e+1) % (epochs/10) == 0:
                     # Run against test set
-                    batch, labels = self.dm.get_batch('test', size=self.batch_size)
-                    output = model(batch).detach().numpy()
-                    correct_output = np.array([float(label == model.label) for label in labels])
-
-                    logging.info('({}/{}) model {}: error = {}'.format(e+1, epochs, i+1, np.mean(np.abs(output - correct_output))))
+                    c_true, o_true = self.test(model)
+                    logging.info('({}/{}) model {}: c_true = {} o_true = {}'.format(e+1, epochs, i, c_true, o_true))
         logging.info('Finish training')
 
 if __name__ == '__main__':
